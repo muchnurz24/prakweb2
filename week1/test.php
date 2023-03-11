90bec37d090db9e22309f5c65e757e1f2a042514
muchnurz24 <114546552+muchnurz24@users.noreply.github.com>
2023-03-11 22:25:33 +0700

    Add files via upload

---
+++ week1/test.php
@@ -0,0 +1,20 @@
+<!-- <?php
+// Definisi Variable
+$nama = "Nurza";
+$nama_depan = "nur";
+
+echo $nama . ' ' . $nama_depan;
+?> -->
+
+
+<?php
+// devinisikan variable
+$nama = "Nurza";
+$umur = 19;
+$berat = 65;
+
+
+echo 'Nama Saya : ' .$nama;
+echo '<br/>Umur Saya : ' .$umur. ' Tahun';
+echo '<br/>Berat Badan : ' .$berat. ' Kg';
+?>
